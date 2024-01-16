beb51a78dcbd63f3a0494c93dbc7dbe2b484ad7d
anisboumdata <127092692+anisboumdata@users.noreply.github.com>
2024-01-16 12:22:39 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+#Init flash APP
+app = Flask(__name__)
+
+#main
+@app.route( '/', methods=['GET'])
+def main():
+    return 'Hello World'
+
+if __name__ == '__main__':
+    app.run("0.0.0.0", port=80)
